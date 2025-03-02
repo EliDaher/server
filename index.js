2bcbc454c99da2713e96f55faa2c6d95f2ae611b
EliDaher <eeli56315@gmail.com>
2025-03-02 17:02:53 +0300

    ignore last commit

--- index.js
+++ index.js
@@ -408,9 +408,8 @@ app.post("/getEmployeeBalance", async (req, res) => {
 });
 
 
-//cron.schedule("0 0 1 * *", async () => {
-    cron.schedule('*/10 * * * *', async () => {
-
+cron.schedule("0 0 1 * *", async () => {
+    
     try {
       const subscribersRef = database.ref("Subscribers");
       const invoicesRef = database.ref("Invoices");
